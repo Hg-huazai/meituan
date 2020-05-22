67164a64df2424473e2d404f59e98ff66ca61775
Huage <2334788801@qq.com>
2020-05-22 15:14:57 +0800

    登录与商家端一样

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,200 +1,407 @@
 <template>
-	<view class="denglu">
-		<view class="login">
-			<view class="name">
-				快捷登录注册
+	<view class="content">
+		<view class="input-group">
+			<view class="inv-h-w">
+				<view :class="['inv-h',Inv==0?'inv-h-se':'']" @click="Inv=0">账号密码登录</view>
+				<view :class="['inv-h',Inv==1?'inv-h-se':'']" @click="Inv=1">手机验证登录</view>
 			</view>
-			<form @submit="formSubmit">
-				<view class="phone">
-					<!-- <text>+86</text>
-					<text></text> -->
-					<input type="text" name="phone" value="" placeholder="请输入用户名"/>
+			<view v-show="Inv == 0">
+				<view class="input-row border">
+					<text class="title">账号：</text>
+					<m-input class="m-input" type="text" clearable focus v-model="account" placeholder="请输入账号"></m-input>
 				</view>
-				<view class="yzm">
-					<input type="text" name="yzm" value="" placeholder="请输入密码"/>
-					<!-- <button type="primary">获取验证码</button> -->
-					<view class="clear">
-					</view>
+				<view class="input-row">
+					<text class="title">密码：</text>
+					<m-input type="password" displayable v-model="password" placeholder="请输入密码"></m-input>
 				</view>
-				<view class="login-button">
-					<button form-type="submit" @click="deluclick">登录</button>
+			</view>
+			<view v-show="Inv == 1">
+				<view class="input-row border">
+					<text class="title">号  码：</text>
+					<m-input class="m-input" type="text" clearable focus v-model="account" placeholder="请输入手机号码"></m-input>
 				</view>
-			</form>
+				<view class="input-row">
+					<text class="title">验证码：</text>
+					<m-input type="password" displayable v-model="password" placeholder="请输入验证码"></m-input>
+				</view>
+			</view>
+		</view>
+		<view class="btn-row">
+			<button type="primary" class="primary" @tap="bindLogin">登录</button> 
+			<!-- <button type="primary" class="primary" @tap="openinfo">登录</button>-->
+		</view>
+		<view class="action-row">
+			<navigator url="../reg/reg">注册账号</navigator>
+			<text>|</text>
+			<navigator url="../pwd/pwd">忘记密码</navigator>
+		</view>
+		<view class="oauth-row" v-if="hasProvider" v-bind:style="{top: positionTop + 'px'}">
+			<view class="oauth-image" v-for="provider in providerList" :key="provider.value">
+				<image :src="provider.image" @tap="oauth(provider.value)"></image>
+				<!-- #ifdef MP-WEIXIN -->
+				<button v-if="!isDevtools" open-type="getUserInfo" @getuserinfo="getUserInfo"></button>
+				<!-- #endif -->
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	// var ab = /^[1][3,4,5,7,8][0-9]{9}$/;
+	import service from '../../service.js';
+	import {
+		mapState,
+		mapMutations
+	} from 'vuex'
+	import mInput from '../../components/m-input.vue'
+
 	export default {
-		methods: {
-			formSubmit(e) {
-				// console.log(e);
-				console.log(e.detail.value.phone)
-				console.log(e.detail.value.yzm)
-				var phone = e.detail.value.phone
-				var yzm = e.detail.value.yzm
-				const type = uni.getStorageSync('type');
-				
-				// var ab = /^[1][3,4,5,7,8][0-9]{9}$/;
-				if (phone == false) {
-					uni.showToast({
-					    title: '请输入用户名',
-					    duration: 2000,
-						icon:'none'
-					});
-				}else {
-					if(!yzm) {
-						// console.log('yzm1')
-						uni.showToast({
-						    title: '请输入密码',
-						    duration: 2000,
-							icon:'none'
-						});
-					}else {
-						getApp().globalData.phone=phone;
-						uni.setStorage({
-						    key: 'phone',
-						    data: phone,
-						    success: function () {
-						        console.log('success');
-						    }
-						});
-						uni.switchTab({
-							url:"../index/index"
-						})
-					}
-				}
-			},
-			deluclick(e){
-				// console.log(e)
+		components: {
+			mInput
+		},
+		data() {
+			return {
+				providerList: [],
+				hasProvider: false,
+				account: '',
+				password: '',
+				positionTop: 0,
+				isDevtools: false,
+				Inv:0,
+				user: '',
 			}
 		},
-		onLoad() {
-			// try {
-			   
-			// } catch (e) {
-			//     // error
-			// }
+		computed: mapState(['forcedLogin']),
+		methods: {
+			...mapMutations(['login']),
+			initProvider() {
+				const filters = ['weixin', 'qq', 'sinaweibo'];
+				uni.getProvider({
+					service: 'oauth',
+					success: (res) => {
+						if (res.provider && res.provider.length) {
+							for (let i = 0; i < res.provider.length; i++) {
+								if (~filters.indexOf(res.provider[i])) {
+									this.providerList.push({
+										value: res.provider[i],
+										image: '../../static/img/' + res.provider[i] + '.png'
+									});
+								}
+							}
+							this.hasProvider = true;
+						}
+					},
+					fail: (err) => {
+						console.error('获取服务供应商失败：' + JSON.stringify(err));
+					}
+				});
+			},
+			initPosition() {
+				/**
+				 * 使用 absolute 定位，并且设置 bottom 值进行定位。软键盘弹出时，底部会因为窗口变化而被顶上来。
+				 * 反向使用 top 进行定位，可以避免此问题。
+				 */
+				this.positionTop = uni.getSystemInfoSync().windowHeight - 100;
+			},
+			bindLogin() {
+				const user = this.user;
+				// console.log(user)
+				/**
+				 * 客户端对账号信息进行一些必要的校验。
+				 * 实际开发中，根据业务需要进行处理，这里仅做示例。
+				 */
+				if (this.account=='') {
+					uni.showToast({
+						icon: 'none',
+						title: '请输入账号'
+					});
+					return;
+				}
+				if (this.password=='') {
+					uni.showToast({
+						icon: 'none',
+						title: '请输入密码'
+					});
+					return;
+				}
+				/**
+				 * 下面简单模拟下服务端的处理
+				 * 检测用户账号密码是否在已注册的用户列表中
+				 * 实际开发中，使用 uni.request 将账号信息发送至服务端，客户端在回调函数中获取结果信息。
+				 */
+				// 缓存拿到注册信息
+				
+				
+				if(this.account == user.account) {
+					console.log('账号对的')
+					if(this.password == user.password){
+						uni.switchTab({
+							url:'../index/index'
+						})
+						uni.showToast({
+							icon: 'none',
+							title: '登录成功'
+						});
+					}else {
+						uni.showToast({
+							icon: 'none',
+							title: '密码错误'
+						});
+					}
+				}else {
+					console.log('账号不存在的')
+					uni.showToast({
+						icon: 'none',
+						title: '账号不存在的'
+					});
+				}
+				
+			},
+			oauth(value) {
+				uni.login({
+					provider: value,
+					success: (res) => {
+						uni.getUserInfo({
+							provider: value,
+							success: (infoRes) => {
+								/**
+								 * 实际开发中，获取用户信息后，需要将信息上报至服务端。
+								 * 服务端可以用 userInfo.openId 作为用户的唯一标识新增或绑定用户信息。
+								 */
+								this.toMain(infoRes.userInfo.nickName);
+							},
+							fail() {
+								uni.showToast({
+									icon: 'none',
+									title: '登陆失败'
+								});
+							}
+						});
+					},
+					fail: (err) => {
+						console.error('授权登录失败：' + JSON.stringify(err));
+					}
+				});
+			},
+			getUserInfo({
+				detail
+			}) {
+				if (detail.userInfo) {
+					this.toMain(detail.userInfo.nickName);
+				} else {
+					uni.showToast({
+						icon: 'none',
+						title: '登陆失败'
+					});
+				}
+			},
+			toMain(userName) {
+				this.login(userName);
+				/**
+				 * 强制登录时使用reLaunch方式跳转过来
+				 * 返回首页也使用reLaunch方式
+				 */
+				if (this.forcedLogin) {
+					uni.reLaunch({
+						url: '../main/main',
+					});
+				} else {
+					uni.navigateBack();
+				}
+
+			},
+			changeTab(Inv){
+					that.navIdx = Inv;
+			},
 			
-			// uni.getStorage({
-			//     key: 'phone',
-			//     success: function (res) {
-			//         console.log(res);
-			//     }
-			// });
+			// 点击登录跳转直接跳转普通页面
+			openinfo() {
+				// console.log('4')
+				uni.switchTab({
+					url: '../order/order'
+				})
+			}
+		},
+		onReady() {
+			this.initPosition();
+			this.initProvider();
+			// #ifdef MP-WEIXIN
+			this.isDevtools = uni.getSystemInfoSync().platform === 'devtools';
+			// #endif
+		},
+		onShow() {
+			const value = uni.getStorageSync('phone');
+			console.log(value);
+			this.user = value
 		}
 	}
 </script>
 
-<style lang="less">
-	.denglu {
+<style>
+	
+	.inv-h-w{background-color: #FFFFFF;height: 100upx;display: flex; border-bottom: 1px solid #C8C7CC;}
+	.inv-h{font-size: 30upx;flex: 1;text-align: center;color: #C9C9C9;height: 100upx;line-height: 100upx;}
+	.inv-h-se{color: #0ec3b4;border-bottom: 4upx solid #0ec3b4;}
+	page{background-color: #F2F2F2;}
+	.action-row {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+
+	.action-row navigator {
+		color: #0ec3b4;
+		padding: 0 10px;
+	}
+
+	.oauth-row {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		position: absolute;
+		top: 0;
+		left: 0;
 		width: 100%;
 	}
-	.logo {
-		width: 160rpx;
-		height: 160rpx;
-		background-color: #FEE3A5;
-		border-radius: 50%;
-		overflow: hidden;
-		line-height: 160rpx;
-		text-align: center;
-		margin-top: 129rpx ;
-		margin-left: 296rpx;
-		font-size: 	36rpx;
-		color: #444A49;
-		font-weight: 600;
-		image {
-			width: 100%;
-			height: 100%;
-		}
+
+	.oauth-image {
+		position: relative;
+		width: 50px;
+		height: 50px;
+		border: 1px solid #dddddd;
+		border-radius: 50px;
+		margin: 0 20px;
+		background-color: #ffffff;
 	}
-	.login {
-		width: 100%;
-		box-sizing: border-box;
-		padding: 0 38rpx;
-		
-		.name {
-			
-			margin-top: 112rpx;
-			font-size: 48rpx;
-			color: #444A49;
-		}
-		form {
-			.phone {
-				margin-top: 30rpx;
-				padding: 20rpx 0;
-				box-sizing: border-box;
-				border-bottom: 2rpx solid #979797;
-				text:nth-child(1) {
-					display: inline-block;
-					vertical-align: middle;
-					font-size: 32rpx;
-					color: #444A49;
-					padding-right: 24rpx;
-				}
-				text:nth-child(2){
-					display: inline-block;
-					vertical-align: middle;
-					width: 2rpx;
-					height: 24rpx;
-					background-color: #979797;
-					margin-right: 24rpx;
-				}
-				input {
-					display: inline-block;
-					vertical-align: middle;
-					font-size: 30rpx;
-				}
-			}
-			.yzm {
-				padding: 10rpx 0;
-				box-sizing: border-box;
-				border-bottom: 2rpx solid #979797;
-				input {
-					display: inline-block;
-					vertical-align: middle;
-					color: #444A49;
-					font-size: 30rpx;
-					margin-top: 20rpx;
-				}
-				button {
-					display: inline-block;
-					vertical-align: middle;
-					width: 184rpx;
-					height: 84rpx;
-					line-height: 84rpx;
-					background-color: #C1B9B2;
-					color: #FFFFFF;
-					font-size: 26rpx;
-					text-align: center;
-					float: right;
-				}
-			}
-			.login-button {
-				margin-top: 92rpx;
-				button {
-					width: 672rpx;
-					height: 96rpx;
-					line-height: 96rpx;
-					background-color: #0ec3b4;
-					font-size: 32rpx;
-				}
-				
-			}
-		}
-		
+
+	.oauth-image image {
+		width: 30px;
+		height: 30px;
+		margin: 10px;
 	}
-	.bottom-tip {
+
+	.oauth-image button {
+		position: absolute;
+		left: 0;
+		top: 0;
 		width: 100%;
-		box-sizing: border-box;
-		padding: 0 38rpx;
-		color: #010101;
-		margin-top: 382rpx;
-		font-size: 22rpx;
-		text:nth-child(2) {
-			// font-size: 18rpx;
-			border-bottom: 2rpx solid #000000;
-		}
+		height: 100%;
+		opacity: 0;
+	}
+	:root {
+	    --my-theme-color: #0ec3b4;
+	}
+	page {
+		min-height: 100%;
+		display: flex;
+		font-size: 16px;
+	}
+	
+	/* #ifdef MP-BAIDU */
+	page {
+		width: 100%;
+		height: 100%;
+		display: block;
+	}
+	
+	swan-template {
+		width: 100%;
+		min-height: 100%;
+		display: flex;
+	}
+	
+	/* 原生组件模式下需要注意组件外部样式 */
+	custom-component {
+		width: 100%;
+		min-height: 100%;
+		display: flex;
+	}
+	
+	/* #endif */
+	
+	/* #ifdef MP-ALIPAY */
+	page {
+		min-height: 100vh;
+	}
+	
+	/* #endif */
+	
+	/* 原生组件模式下需要注意组件外部样式 */
+	m-input {
+		width: 100%;
+		/* min-height: 100%; */
+		display: flex;
+		flex: 1;
+	}
+	
+	.content {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		background-color: #f6f6fa;
+		padding: 10px;
+	}
+	
+	.input-group {
+		background-color: #ffffff;
+		margin-top: 20px;
+		position: relative;
+	}
+	
+	.input-group::before {
+		position: absolute;
+		right: 0;
+		top: 0;
+		left: 0;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
+	
+	.input-group::after {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
+	
+	.input-row {
+		display: flex;
+		flex-direction: row;
+		position: relative;
+		font-size: 18px;
+		line-height: 40px;
+	}
+	
+	.input-row .title {
+		width: 72px;
+		padding-left: 15px;
+	}
+	
+	.input-row.border::after {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 8px;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
+	
+	.btn-row {
+		margin-top: 25px;
+		padding: 10px;
+	}
+	
+	button.primary {
+		background-color: #0ec3b4;
 	}
 </style>
